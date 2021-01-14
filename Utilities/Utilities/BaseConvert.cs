d68b58ea23ffd222352403b6d05dbadd5aa2fbfa
Matthew Lothamer <mclotham@hotmail.com>
2021-01-13 22:03:26 -0500

    Initial commit

---
+++ Utilities/Utilities/BaseConvert.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Mcl.Utilities
+{
+    class BaseConvert
+    {
+        /// <summary>
+        /// Caution when using the constants for base digits. Pay attention to case sensitivity.
+        /// Example:
+        /// when using Base16Digits
+        ///     string myString = "01fa10";
+        ///     Int64 myNumber = BaseConvert.FromBase( myString, BaseConvert.Base16Digits );
+        /// will cause an exception.
+        /// Instead use
+        ///     string myString = "01fa10";
+        ///     Int64 myNumber = BaseConvert.FromBase( myString.ToUpper(), BaseConvert.Base16Digits );
+        /// </summary>
+        public const string Base16Digits = "0123456789ABCDEF";
+        public const string Base32Digits = "0123456789ABCDEFGHJKLMNPRTUVWXYZ";
+        public const string Base36Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string Base62Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        public const string Base64Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+
+        public static string ToBase(UInt64 inputValue, string baseDigits, int numPadChars)
+        {
+            if (baseDigits.ContainsDuplicateChars())
+                throw new BaseConvertException ( "Base Digits string contains one or more duplicates: " + baseDigits );
+
+            StringBuilder builder = new StringBuilder();
+
+            while (inputValue > 0)
+            {
+                int remainder = (int)(inputValue % (UInt64)(baseDigits.Length));
+                builder.Insert(0, baseDigits[remainder]);
+
+                inputValue /= (UInt64)baseDigits.Length;
+            }
+
+            return builder.ToString().PadLeft(numPadChars, baseDigits[0]);
+        }
+
+        public static string ToBase(BigInteger inputValueString, string baseDigits, int numPadChars)
+        {
+            if (baseDigits.ContainsDuplicateChars())
+                throw new BaseConvertException("Base Digits string contains one or more duplicates: " + baseDigits);
+
+            StringBuilder builder = new StringBuilder();
+
+            while (inputValueString > 0)
+            {
+                int remainder = (int)(inputValueString % (baseDigits.Length));
+                builder.Insert(0, baseDigits[remainder]);
+
+                inputValueString /= baseDigits.Length;
+            }
+
+            return builder.ToString().PadLeft(numPadChars, baseDigits[0]);
+        }
+
+        public static UInt64 FromBase(string inputValueString, string baseCharacters)
+        {
+            UInt64 outnum = 0;
+            long power = 0;
+
+            while (inputValueString.Length > 0)
+            {
+                int index = (int)Array.IndexOf<char>(baseCharacters.ToCharArray(), inputValueString[inputValueString.Length - 1]);
+
+                //InputString contains a character that doesn't exist in BaseCharacters. Throw an exception
+                if (index == -1)
+                    throw new BaseConvertException("The input string contains a digit that is not a base digit.");
+
+                outnum += ((uint)index * (UInt64)Math.Pow(baseCharacters.Length, power));
+                inputValueString = inputValueString.Remove(inputValueString.Length - 1);
+                power++;
+            }
+
+            return outnum;
+        }
+
+        public static BigInteger FromBaseBigInteger(string inputString, string baseCharacters)
+        {
+            BigInteger outnum = 0;
+            long power = 0;
+
+            while (inputString.Length > 0)
+            {
+                int index = (int)Array.IndexOf<char>(baseCharacters.ToCharArray(), inputString[inputString.Length - 1]);
+
+                //InputString contains a character that doesn't exist in BaseCharacters. Throw an exception
+                if (index == -1)
+                    throw new BaseConvertException("The input string contains a digit that is not a base digit.");
+
+                outnum += ((uint)index * (BigInteger)Math.Pow(baseCharacters.Length, power));
+                inputString = inputString.Remove(inputString.Length - 1);
+                power++;
+            }
+
+            return outnum;
+        }
+
+        public class BaseConvertException : Exception
+        {
+            public BaseConvertException()
+            {
+            }
+
+            public BaseConvertException(string message)
+                : base(message)
+            {
+            }
+
+            public BaseConvertException(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+        }
+
+    }
+}
---
+++ Utilities/Utilities/StringExtensions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mcl.Utilities
+{
+    public enum HandleLongWords
+    {
+        CutOff,
+        Allow,
+        ThrowException,
+    }
+
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Counts the number of occurrences of a specific character in a string
+        /// </summary>
+        /// <param name="str">This string</param>
+        /// <param name="character">The character to be counted</param>
+        /// <returns>
+        /// The number of occurrences of the specified character
+        /// </returns>
+        public static int SpecificCharCount( this string str, char character )
+        {
+            var charCount = 0;
+
+            foreach (char c in str)
+                if (c == character)
+                    charCount++;
+
+            return charCount;
+        }
+
+        /// <summary>
+        /// Determines if there are duplicate characters in the string
+        /// </summary>
+        /// <param name="str">This string</param>
+        /// <returns>
+        /// <c>true</c> if there are duplicate(s) of any character in the string
+        /// </returns>
+        public static bool ContainsDuplicateChars( this string str)
+        {
+            foreach (char c in str)
+                if (str.SpecificCharCount(c) > 1)
+                    return true;
+
+            return false;
+        }
+
+        public static byte[] ToByteArray(this string str)
+        {
+            return Encoding.ASCII.GetBytes(str);
+        }
+
+        /// <summary>
+        /// Breaks the string into 1 or more strings based on the delimiter and the maximum number of characters per line
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// string myString = "It has truely been a pleasure writing this method for you.";
+        /// string[] myLines = myString.LineBreak(" ", 15, HandleLongWords.CutOff);
+        ///     foreach (string line in myLines)
+        ///         Console.WriteLine(line);
+        /// </code>
+        /// Output:
+        /// It has truely
+        /// been a pleasure
+        /// writing this
+        /// method for you.
+        /// </example>
+        /// <param name="str">This string</param>
+        /// <param name="separator">Separator or delimitter between words</param>
+        /// <param name="maxLength">Maximum length of each string in the returned array</param>
+        /// <param name="handleLongWords">Flag indicating how to handle words longer than maxLength</param>
+        /// <returns>An array of strings</returns>
+        /// <exception cref="LineBreakException">Thrown when <c>handleLongWords = HandleLongWords.ThrowException</c> 
+        /// and a string token in the original string is longer than <c>maxLength</c></exception>
+        public static string[] LineBreak(this String str, string separator, int maxLength, HandleLongWords handleLongWords = HandleLongWords.CutOff)
+        {
+            if (str == null)
+                return new string[0];
+
+            var tokens = str.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+            var lines = new List<string>();
+
+            var line = "";
+            for (var tokenIdx = 0; tokenIdx < tokens.Length; tokenIdx++)
+            {
+                var tmpBrkStr = (lines.Count() == 0 && (line.Length == 0 || line == separator)) ? "" : separator;
+                if ((line + tmpBrkStr + tokens[tokenIdx]).Length <= maxLength)
+                {
+                    line += tmpBrkStr + tokens[tokenIdx];
+                    if (tokenIdx >= tokens.Length - 1)
+                        lines.Add(line);
+                }
+                else
+                if (tokens[tokenIdx].Length > maxLength)
+                {
+                    if (handleLongWords == HandleLongWords.Allow || handleLongWords == HandleLongWords.CutOff)
+                    {
+                        if (line.Length > 0)
+                            lines.Add(line);
+                        line = tokens[tokenIdx];
+                        if (handleLongWords == HandleLongWords.CutOff && line.Length > maxLength)
+                            line = line.Substring(0, maxLength);
+                        if (tokenIdx >= tokens.Length - 1)
+                            lines.Add(line);
+                    }
+                    else
+                        throw new LineBreakException( $"String contains word \'{tokens[tokenIdx]}\' longer that maxLength {maxLength}");
+                }
+                else
+                {
+                    if (line.Length > 0)
+                        lines.Add(line);
+                    line = tokens[tokenIdx];
+                    if (tokenIdx >= tokens.Length - 1)
+                        lines.Add(line);
+                }
+            }
+
+            return lines.ToArray();
+        }
+
+        public class LineBreakException : Exception
+        {
+            public LineBreakException()
+            {
+            }
+
+            public LineBreakException(string message)
+                : base(message)
+            {
+            }
+
+            public LineBreakException(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+        }
+
+    }
+}
