ad7409e8764de5ec40a09d528ec8a6e44e289610
mclotham <77400805+mclotham@users.noreply.github.com>
2021-02-04 09:40:42 -0500

    Added duplicate base character exception to FromBase and FromBaseBigInteger. Added inline documentation

--- Utilities/Utilities/BaseConvert.cs
+++ Utilities/Utilities/BaseConvert.cs
@@ -27,12 +27,15 @@ namespace Mcl.Utilities
         public const string Base64Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
 
         /// <summary>
-        /// Converts an unsigned integer to
+        /// Converts an unsigned integer to the specified number base as a string representation
         /// </summary>
-        /// <param name="inputValue"></param>
-        /// <param name="baseDigits"></param>
-        /// <param name="numPadChars"></param>
-        /// <returns></returns>
+        /// <param name="inputValue">Integer value to convert</param>
+        /// <param name="baseDigits">Number base digits</param>
+        /// <param name="numPadChars">Minimum number of characters for returned string</param>
+        /// <returns>
+        /// String representation of converted number
+        /// </returns>
+        /// <exception cref="BaseConvertException">Thrown if <c>baseDigits</c> contains one or more duplicate characters</exception>
         public static string ToBase( UInt64 inputValue, string baseDigits, int numPadChars = 0 )
         {
             if ( baseDigits.ContainsDuplicateChars() )
@@ -54,6 +57,16 @@ namespace Mcl.Utilities
             return builder.ToString().PadLeft( numPadChars, baseDigits[0] );
         }
 
+        /// <summary>
+        /// Converts an unsigned integer to the specified number base as a string representation
+        /// </summary>
+        /// <param name="inputValue">Integer value to convert</param>
+        /// <param name="baseDigits">Number base digits</param>
+        /// <param name="numPadChars">Minimum number of characters for returned string</param>
+        /// <returns>
+        /// String representation of converted number
+        /// </returns>
+        /// <exception cref="BaseConvertException">Thrown if <c>baseDigits</c> contains one or more duplicate characters or if <c>inputValue is negative</c></exception>
         public static string ToBase( BigInteger inputValue, string baseDigits, int numPadChars = 0 )
         {
             if ( inputValue.Sign == -1 )
@@ -77,20 +90,32 @@ namespace Mcl.Utilities
             return builder.ToString().PadLeft( numPadChars, baseDigits[0] );
         }
 
-        public static UInt64 FromBase(string inputValueString, string baseCharacters)
+        /// <summary>
+        /// Converts a string representation of a value from its number base to an unsigned integer
+        /// </summary>
+        /// <param name="inputValueString">String representation of the value to convert</param>
+        /// <param name="baseDigits">Number base digits</param>
+        /// <returns>
+        /// Converted unsigned integer
+        /// </returns>
+        /// <exception cref="BaseConvertException">Thrown if <c>baseDigits</c> contains one or more duplicate characters or if <c>inputValueString contains digits that are not base digits</c></exception>
+        public static UInt64 FromBase(string inputValueString, string baseDigits)
         {
+            if ( baseDigits.ContainsDuplicateChars() )
+                throw new BaseConvertException( "Base Digits string contains one or more duplicates: " + baseDigits );
+
             UInt64 outnum = 0;
             int power = 0;
 
-            while (inputValueString.Length > 0)
+            while ( inputValueString.Length > 0)
             {
-                int index = Array.IndexOf<char>(baseCharacters.ToCharArray(), inputValueString[inputValueString.Length - 1]);
+                int index = Array.IndexOf<char>( baseDigits.ToCharArray(), inputValueString[inputValueString.Length - 1]);
 
                 //InputString contains a character that doesn't exist in BaseCharacters. Throw an exception
                 if (index == -1)
-                    throw new BaseConvertException($"The input string {inputValueString} contains digits that are not base digits. Base digits: {baseCharacters}.");
+                    throw new BaseConvertException($"The input string {inputValueString} contains digits that are not base digits. Base digits: {baseDigits}.");
 
-                outnum += ((uint)index * (UInt64)Math.Pow(baseCharacters.Length, power));
+                outnum += ((uint)index * (UInt64)Math.Pow( baseDigits.Length, power));
                 inputValueString = inputValueString.Remove(inputValueString.Length - 1);
                 power++;
             }
@@ -98,20 +123,32 @@ namespace Mcl.Utilities
             return outnum;
         }
 
-        public static BigInteger FromBaseBigInteger( string inputValueString, string baseCharacters )
+        /// <summary>
+        /// Converts a string representation of a value from its number base to an unsigned integer
+        /// </summary>
+        /// <param name="inputValueString">String representation of the value to convert</param>
+        /// <param name="baseDigits">Number base digits</param>
+        /// <returns>
+        /// Converted unsigned integer
+        /// </returns>
+        /// <exception cref="BaseConvertException">Thrown if <c>baseDigits</c> contains one or more duplicate characters or if <c>inputValueString contains digits that are not base digits</c></exception>
+        public static BigInteger FromBaseBigInteger( string inputValueString, string baseDigits )
         {
+            if ( baseDigits.ContainsDuplicateChars() )
+                throw new BaseConvertException( "Base Digits string contains one or more duplicates: " + baseDigits );
+
             BigInteger outnum = 0;
             int power = 0;
 
             while ( inputValueString.Length > 0 )
             {
-                int index = Array.IndexOf<char>( baseCharacters.ToCharArray(), inputValueString[inputValueString.Length - 1] );
+                int index = Array.IndexOf<char>( baseDigits.ToCharArray(), inputValueString[inputValueString.Length - 1] );
 
                 //InputString contains a character that doesn't exist in BaseCharacters. Throw an exception
                 if ( index == -1 )
-                    throw new BaseConvertException( $"The input string {inputValueString} contains digits that are not base digits. Base digits: {baseCharacters}." );
+                    throw new BaseConvertException( $"The input string {inputValueString} contains digits that are not base digits. Base digits: {baseDigits}." );
 
-                outnum = BigInteger.Add( outnum, (BigInteger)index * BigInteger.Pow( baseCharacters.Length, power ) );
+                outnum = BigInteger.Add( outnum, (BigInteger)index * BigInteger.Pow( baseDigits.Length, power ) );
                 inputValueString = inputValueString.Remove( inputValueString.Length - 1 );
                 power++;
             }
